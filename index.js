5e056653e085e1741599123a00a6c15bd7111a8a
Seth Wright <asethwright@gmail.com>
2018-08-30 16:31:52 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ class Eslint {
             Dependencies have been installed. Please run "npm run dev" again.
         `;
 
-        return ['eslint', 'eslint-loader'];
+        return ['eslint', 'eslint-loader', 'eslint-plugin-vue'];
     }
 
     register(options = {}) {
